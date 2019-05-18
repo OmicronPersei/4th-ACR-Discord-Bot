66104a124f89a727501a8f3f38efe148dbfa0a22
Wheeler Weise <wheeler.weise@gmail.com>
2019-05-18 16:53:28 -0500

    Created a welcome message implementation.
    Start of new feature to automatically resolve and mention Members.  Unit test not yet passing.
    
    Trying to get unit tests to work, still failing.
    
    Unit tests now pass.
    
    Test refactoring, no longer working
    
    Tests pass!
    
    Some refactoring to compensate for the fact I'm doing asyncio programming, works so far in testing.
    
    Fixes test to actually work async, handles the {user} mapping.
    
    Added functionality for replacing/mentioning roles in the welcome message.
    
    Refactored user mention to make use of object's mention property
    
    More refactoring of a unit test!
    
    refactoring
    
    Got tests passing again.
    
    Some refactoring.

--- discord_service.py
+++ discord_service.py
@@ -2,17 +2,24 @@ import discord
 
 class DiscordService(discord.Client):
     def __init__(self):
-        super().__init__(self)
+        super().__init__()
         self.on_member_join_callbacks = []
 
-    def on_member_join(self, member):
+    async def on_member_join(self, member):
         for callback in self.on_member_join_callbacks:
-            callback(member)
+            await callback(member)
 
-    def send_channel_message(self, message, channel):
-        pass
+    async def send_channel_message(self, message, channel_name):
+        channels = self.get_all_channels()
+        channel = [x for x in channels if x.name == channel_name][0]
+        await channel.send(message)
 
-    def get_matching_Member(self, display_name):
+    def get_matching_Member(self, username, discriminator):
         all_members = self.get_all_members()
-        matching_member = [x for x in all_members if x.display_name == display_name][0]
+
+        matching_member = [x for x in all_members if x.name == username and x.discriminator == discriminator][0]
         return matching_member
+
+    def get_matching_role(self, role_name):
+        all_roles = self.guilds[0].roles
+        return [x for x in all_roles if x.name == role_name][0]
--- welcome_message_test.py
+++ welcome_message_test.py
@@ -1,38 +1,141 @@
 import unittest
-from unittest.mock import MagicMock
-from welcome_message import *
-from discord import User
+import asyncio
+import asynctest
+from unittest.mock import Mock, MagicMock, PropertyMock, call
+from welcome_message import WelcomeMessage
+from discord_service import DiscordService
 
-class Tests(unittest.TestCase):
-    def test_sets_up_callback_for_discord_message(self):
-        discord = MagicMock()
-        join_callbacks = []
-        discord.on_member_join_callbacks = join_callbacks
-        
-        WelcomeMessage(None, discord)
+def create_mock_user(mention_val):
+    mock_user = MagicMock()
+    type(mock_user).mention = PropertyMock(return_value=mention_val)
+    return mock_user
 
-        assert len(join_callbacks) == 1
-        assert callable(join_callbacks[0])
+class TestConstructionSubscribesToOnMemberJoinCallback(asynctest.TestCase):
+    def setUp(self):
+        self.discord = MagicMock()
+        self.join_callbacks = []
+        self.discord.on_member_join_callbacks = self.join_callbacks
 
-    def test_says_message_on_member_join_callback(self):
-        discord = MagicMock()
-        discord.on_member_join_callbacks = []
-        discord.send_channel_message = MagicMock()
-        config = {
+    def test(self):
+        WelcomeMessage(None, self.discord)
+
+        assert len(self.join_callbacks) == 1
+        assert callable(self.join_callbacks[0])
+
+class TestSendsChannelMessageUponOnMemberJoinCallback(asynctest.TestCase):
+    def setUp(self):
+        self.discord = asynctest.Mock(DiscordService())
+        self.discord.on_member_join_callbacks = []
+        self.config = {
             "message": "my message",
             "channel": "my channel"
         }
-        user_joined = User(state=MagicMock(), data=MagicMock())
-        WelcomeMessage(config, discord)
+        self.user_joined = create_mock_user("val")
+        WelcomeMessage(self.config, self.discord)
 
-        callback = discord.on_member_join_callbacks[0]
-        callback(user_joined)
+    async def test(self):
+        callback = self.discord.on_member_join_callbacks[0]
+        await callback(self.user_joined)
 
-        discord.send_channel_message.assert_called_once_with(message="my message", channel="my channel")
+        self.discord.send_channel_message.assert_called_once_with("my message", "my channel")
 
-    
+class TestSendsChannelMessageUponOnMemberJoinCallbackWithUsernameReplacedWithMentions(asynctest.TestCase):
+    def setUp(self):
+        self.discord = asynctest.Mock(DiscordService())
+        self.discord.on_member_join_callbacks = []
+        self.discord.send_channel_message = MagicMock()
+        self.discord.send_channel_message.return_value = asyncio.Future()
+        self.discord.send_channel_message.return_value.set_result(None)
 
+        self.mock_user_exe_mention="<exe_str>"
+        self.mock_user_Exe = create_mock_user(self.mock_user_exe_mention)
+        self.mock_user_dabele_mention="<abele_str>"
+        self.mock_user_DAbele = create_mock_user(self.mock_user_dabele_mention)
 
+        self.mock_joining_user = create_mock_user("val")
+        
+        self.discord.get_matching_Member = MagicMock()
+        self.discord.get_matching_Member.side_effect = self.get_matching_member_side_effect
+        
+        config = {
+            "message": "{member:Exe#1234} welcomes you!  Unit of {member:D. Abele#789}",
+            "channel": "my channel"
+        }
+        WelcomeMessage(config, self.discord)
+
+    def get_matching_member_side_effect(self, *args, **kwargs):
+        if args[0] == "Exe" and args[1] == "1234":
+            return self.mock_user_Exe
+        elif args[0] == "D. Abele" and args[1] == "789":
+            return self.mock_user_DAbele
+
+    async def test(self):
+        callback = self.discord.on_member_join_callbacks[0]
+        await callback(self.mock_joining_user)
+
+        mock = self.discord.get_matching_Member
+        expected_calls = [call("Exe", "1234"), call("D. Abele", "789")]
+        mock.assert_has_calls(expected_calls, any_order=True)
+
+        expected_message = "{} welcomes you!  Unit of {}".format(self.mock_user_exe_mention, self.mock_user_dabele_mention)
+        expected_channel = "my channel"
+        self.discord.send_channel_message.assert_called_once_with(expected_message, expected_channel)
+
+class TestSendsChannelMessageUponJoinCallbackAndResolvesJoiningUser(asynctest.TestCase):
+    def setUp(self):
+        self.discord = asynctest.Mock(DiscordService())
+        self.discord.on_member_join_callbacks = []
+        self.discord.send_channel_message = MagicMock()
+        self.discord.send_channel_message.return_value = asyncio.Future()
+        self.discord.send_channel_message.return_value.set_result(None)
+
+        self.mock_joining_user_mention_val = "<val!>"
+        self.mock_joining_user = create_mock_user(self.mock_joining_user_mention_val)
+        
+        config = {
+            "message": "Welcome, {joined_user}!",
+            "channel": "my channel"
+        }
+        WelcomeMessage(config, self.discord)
+
+    async def test(self):
+        callback = self.discord.on_member_join_callbacks[0]
+        await callback(self.mock_joining_user)
+
+        self.discord.get_matching_Member.assert_not_called()
+
+        expected_message = "Welcome, {}!".format(self.mock_joining_user_mention_val)
+        expected_channel = "my channel"
+        self.discord.send_channel_message.assert_called_once_with(expected_message, expected_channel)
+
+class TestWelcomeMessageResolvesRoleMention(asynctest.TestCase):
+    def setUp(self):
+        self.discord = asynctest.Mock(DiscordService())
+        self.discord.on_member_join_callbacks = []
+        self.discord.send_channel_message = MagicMock()
+        self.discord.send_channel_message.return_value = asyncio.Future()
+        self.discord.send_channel_message.return_value.set_result(None)
+
+        self.mock_joining_user = create_mock_user("<val>")
+
+        matching_role = MagicMock()
+        type(matching_role).mention = PropertyMock(return_value="<rolestr>")
+        self.discord.get_matching_role = MagicMock(return_value=matching_role)
+        
+        config = {
+            "message": "Please message a {role:Recruiter}!",
+            "channel": "my channel"
+        }
+        WelcomeMessage(config, self.discord)
+
+    async def test(self):
+        callback = self.discord.on_member_join_callbacks[0]
+        await callback(self.mock_joining_user)
+
+        expected_message = "Please message a <rolestr>!"
+        expected_channel = "my channel"
+        self.discord.get_matching_role.assert_called_once_with("Recruiter")
+        self.discord.send_channel_message.assert_called_once_with(expected_message, expected_channel)
 
 
 if __name__ == '__main__':
