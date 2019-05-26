a4bc46f9797549e202c2f68c7d08d3de3e5891b4
Wheeler Weise <wheeler.weise@gmail.com>
2019-05-26 10:07:23 -0500

    Adds dependency injection and refactors the bot to use it.
    Initial work of setting up dependency injection.
    
    test, still not working
    
    Appears to be working
    
    Appears to be working and is now configurable.
    
    Added unit testing for dependency injection
    
    Some refactoring.

--- bot.py
+++ bot.py
@@ -2,37 +2,40 @@ from discord_service import DiscordService
 from welcome_message import WelcomeMessage
 from discord_mention_factory import DiscordMentionFactory
 from user_leave_notification import UserLeaveNotification
+
+from dependency_injection import Dependencies
 import json
 
 def readJsonFile(file_name):
     with open(file_name, mode="r") as f:
         return json.load(f)
 
-def read_config():
-    return readJsonFile("config.json")
-
 def read_secrets():
     return readJsonFile("secrets.json")
 
+def read_config():
+    return readJsonFile("config.json")
+
+def setup_dependency_injection(config):
+    return Dependencies(config)
 
 if __name__ == "__main__":
     config = read_config()
-
     secrets = read_secrets()
     discord_token = secrets["discord-bot-token"]
-    
-    discord_service = DiscordService()
 
-    discord_mention_factory = DiscordMentionFactory(discord_service)
+    services = setup_dependency_injection(config)
 
-    welcome_message_config = config["welcome_message"]
-    if welcome_message_config["enabled"]:
-        welcome_message = WelcomeMessage(welcome_message_config, discord_service, discord_mention_factory)
+    if config["welcome_message"]["enabled"]:
+        services.welcome_message()
 
-    user_leave_config = config["user_leave_notification"]
-    if user_leave_config["enabled"]:
-        user_leave_notification = UserLeaveNotification(user_leave_config, discord_service, discord_mention_factory)
+    if config["user_leave_notification"]["enabled"]:
+        services.user_leave_notification()
 
+    discord_service = services.discord_service()
     discord_service.run(discord_token)
+
+
+
 
     
---
+++ dependency_injection.py
@@ -0,0 +1,18 @@
+from dependency_injector import providers
+
+from discord_service import DiscordService
+from discord_mention_factory import DiscordMentionFactory
+from user_leave_notification import UserLeaveNotification
+from welcome_message import WelcomeMessage
+
+class Dependencies:
+    def __init__(self, config):
+        super().__init__()
+        self.config = providers.Configuration("config")
+        self.config.override(config)
+
+        self.discord_service = providers.Singleton(DiscordService)
+        self.discord_mention_factory = providers.Singleton(DiscordMentionFactory, self.discord_service)
+        self.welcome_message = providers.Singleton(WelcomeMessage, self.config.welcome_message, self.discord_service, self.discord_mention_factory)
+        self.user_leave_notification = providers.Singleton(UserLeaveNotification, self.config.user_leave_notification, self.discord_service, self.discord_mention_factory)
+
