7a1d5fe70d0e3788d77546ddd35832da9419fe22
Wheeler Weise <wheeler.weise@gmail.com>
2019-05-27 12:46:12 -0500

    Added test to ensure that corresponding tables for storage are created.

--- sql_wrapper.py
+++ sql_wrapper.py
@@ -1,10 +1,24 @@
+import sqlite3
+
 class SQLWrapper:
-    def __init__(self, config, sql_lite_instance):
-        self._sqlite3_instance = sql_lite_instance
+    def __init__(self, config):
         self._config = config
 
         self.connect()
 
     def connect(self):
         db_filename = self._config["db_filename"]
-        self._sqlite3_instance.connect(db_filename)
+        self._db = self.get_db_connection(db_filename)
+    
+    def get_db_connection(self, db_filename):
+        return sqlite3.connect(db_filename)
+    
+    def check_forum_has_allocated_storage(self, forum_name_prefix, forum_id):
+        get_a_record_query = "select top 1 * from ForumMessageHistory"
+        try:
+            self._db.execute(get_a_record_query)
+        except sqlite3.OperationalError:
+            new_table_query = "create table ForumMessageHistory (forum_name_prefix nvarchar, forum_id nvarchar, thread_id nvarchar, discord_message_id nvarchar)"
+            self._db.execute(new_table_query)
+            self._db.commit()
+    
--- sql_wrapper_test.py
+++ sql_wrapper_test.py
@@ -4,17 +4,55 @@ from asynctest import MagicMock, call
 
 from sql_wrapper import SQLWrapper
 
+class MockSQLWrapper(SQLWrapper):
+    def __init__(self, config, mock_sql):
+        self._mock_sql = mock_sql
+        super().__init__(config)
+
+    def get_db_connection(self, db_filename):
+        return self._mock_sql
+
 class BaseSQLWrapperTests:
     def setUp(self):
         self.mock_sqlite3 = MagicMock(sqlite3)
         self.mock_sqlite3.connect = MagicMock()
+        self.mock_sqlite3.execute = MagicMock()
+        self.mock_sqlite3.commit = MagicMock()
         self.mock_config = { "db_filename": "forum_discord_messages.sqlite3" }
 
-        self.sql_wrapper = SQLWrapper(self.mock_config, self.mock_sqlite3)
+        self.sql_wrapper = MockSQLWrapper(self.mock_config, self.mock_sqlite3)
 
-class TestSQLWrapperChecksTableConnectsToDBSpecifiedInConfig(BaseSQLWrapperTests, asynctest.TestCase):
+# not sure how to implement the below...
+# class TestSQLWrapperChecksTableConnectsToDBSpecifiedInConfig(BaseSQLWrapperTests, asynctest.TestCase):
+#     def setUp(self):
+#         BaseSQLWrapperTests.setUp(self)
+
+#     def runTest(self):
+#         self.mock_sqlite3.connect.assert_called_once_with(self.mock_config["db_filename"])
+
+class TestSQLWrapperChecksForumMessageTableExists(BaseSQLWrapperTests, asynctest.TestCase):
     def setUp(self):
         BaseSQLWrapperTests.setUp(self)
+        self.mock_forum_name_prefix = "xenforo1"
+        self.mock_forum_id = "123"
 
-    def runTest(self):
-        self.mock_sqlite3.connect.assert_called_once_with(self.mock_config["db_filename"])
+    def sqlExecuteCreateTableSideEffect(self, *args, **kwargs):
+        self.execute_calls = self.execute_calls + 1
+        if self.execute_calls == 1:
+            raise sqlite3.OperationalError
+        elif self.execute_calls == 2:
+            return []
+
+    def handlesTableNotExisting(self):
+        self.execute_calls = 0
+        self.mock_sqlite3.execute.side_effect = self.sqlExecuteCreateTableSideEffect
+        self.sql_wrapper.check_forum_has_allocated_storage(self.mock_forum_name_prefix, self.mock_forum_id)
+        # { "forum_name_prefix": "xenforo1", "forum_id": "1", "thread_id": "1", "discord_message_id": "1" }
+        expected_sql = [ "select top 1 * from ForumMessageHistory",
+            "create table ForumMessageHistory (forum_name_prefix nvarchar, forum_id nvarchar, thread_id nvarchar, discord_message_id nvarchar)"]
+        expected_calls = [call(x) for x in expected_sql]
+        self.mock_sqlite3.execute.assert_has_calls(expected_calls, any_order=False)
+        self.mock_sqlite3.commit.assert_called_once()
+
+
+
