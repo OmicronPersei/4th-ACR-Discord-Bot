84f23d1e0e22c22b293f0b82410c1aa6883bb555
Wheeler Weise <wheeler.weise@gmail.com>
2019-05-27 11:14:49 -0500

    Woo another method

--- forum_thread_data_storage.py
+++ forum_thread_data_storage.py
@@ -7,3 +7,6 @@ class ForumThreadDataStorage:
     def check_forums_have_allocated_storage(self, forum_threads):
         for thread in forum_threads:
             self._db.check_forum_has_allocated_storage(thread["forum_name_prefix"], thread["forum_id"])
+
+    def get_forum_thread_records(self, forum_thread_query):
+        return self._db.get_forum_records(forum_thread_query["forum_name_prefix"], forum_thread_query["forum_id"])
--- forum_thread_data_storage_test.py
+++ forum_thread_data_storage_test.py
@@ -11,15 +11,47 @@ mock_forum_threads = [ {
         "forum_id": "2"
     }]
 
-class TestForumDataStorageChecksIfTablesExistsUponConstructionAndDoesNothingWhenExist(asynctest.TestCase):
+class ForumThreadDataStorageTestBase:
     def setUp(self):
+        self.mock_forum_threads = [ {
+                "forum_name_prefix": "xenforo1",
+                "forum_id": "1"
+            }, 
+            {
+                "forum_name_prefix": "xenforo1",
+                "forum_id": "2"
+            }]
         self.mock_sql = MagicMock()
         self.mock_sql.check_forum_has_allocated_storage = MagicMock()
         self.forum_thread_data_storage = ForumThreadDataStorage(self.mock_sql, mock_forum_threads)
 
+class TestForumDataStorageChecksIfTablesExistsUponConstructionAndDoesNothingWhenExist(ForumThreadDataStorageTestBase, asynctest.TestCase):
+    def setUp(self):
+        ForumThreadDataStorageTestBase.setUp(self)
+
     def runTest(self):
         expected_calls = [call("xenforo1", "1"), call("xenforo1", "2")]
         self.mock_sql.check_forum_has_allocated_storage.assert_has_calls(expected_calls, any_order=True)
+
+class TestForumDataStorageReturnsExistingForumThreadRecords(ForumThreadDataStorageTestBase, asynctest.TestCase):
+    def setUp(self):
+        ForumThreadDataStorageTestBase.setUp(self)
+        
+        self.forum_id1_records = [
+            { "forum_name_prefix": "xenforo1", "forum_id": "1", "thread_id": "1", "discord_message_id": "1" }
+        ]
+        self.mock_sql.get_forum_records = MagicMock(return_value=self.forum_id1_records)
+    
+    def runTest(self):
+        forum_thread_query = { "forum_name_prefix": "xenforo1", "forum_id": "1" }
+        actual = self.forum_thread_data_storage.get_forum_thread_records(forum_thread_query)
+
+        self.mock_sql.get_forum_records.assert_called_once_with("xenforo1", "1")
+        assert len(actual) == 1
+        assert actual == self.forum_id1_records
+    
+
+
         
 
 
