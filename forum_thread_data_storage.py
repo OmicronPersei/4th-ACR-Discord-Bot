6e44378d47a46c928140d43756c8a570e542d032
Wheeler Weise <wheeler.weise@gmail.com>
2019-05-27 10:00:13 -0500

    Refactored test

--- forum_thread_data_storage.py
+++ forum_thread_data_storage.py
@@ -6,4 +6,4 @@ class ForumThreadDataStorage:
         
     def check_forums_have_allocated_storage(self, forum_threads):
         for thread in forum_threads:
-            self._db.check_forum_has_allocated_storage(thread["forum_name_prefix"], thread["thread_id"])
+            self._db.check_forum_has_allocated_storage(thread["forum_name_prefix"], thread["forum_id"])
--- forum_thread_data_storage_test.py
+++ forum_thread_data_storage_test.py
@@ -2,19 +2,20 @@ from forum_thread_data_storage import ForumThreadDataStorage
 import asynctest
 from asynctest import MagicMock, call
 
+mock_forum_threads = [ {
+        "forum_name_prefix": "xenforo1",
+        "forum_id": "1"
+    }, 
+    {
+        "forum_name_prefix": "xenforo1",
+        "forum_id": "2"
+    }]
+
 class TestForumDataStorageChecksIfTablesExistsUponConstructionAndDoesNothingWhenExist(asynctest.TestCase):
     def setUp(self):
-        self.forum_threads = [ {
-                "forum_name_prefix": "xenforo1",
-                "thread_id": "1"
-            }, 
-            {
-                "forum_name_prefix": "xenforo1",
-                "thread_id": "2"
-            }]
         self.mock_sql = MagicMock()
         self.mock_sql.check_forum_has_allocated_storage = MagicMock()
-        self.forum_thread_data_storage = ForumThreadDataStorage(self.mock_sql, self.forum_threads)
+        self.forum_thread_data_storage = ForumThreadDataStorage(self.mock_sql, mock_forum_threads)
 
     def runTest(self):
         expected_calls = [call("xenforo1", "1"), call("xenforo1", "2")]
