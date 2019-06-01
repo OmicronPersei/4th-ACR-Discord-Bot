99499f0c71a012a04b0f8e7d63eb84be495c0bf3
Wheeler Weise <wheeler.weise@gmail.com>
2019-05-31 21:01:35 -0500

    Here have more tests

--- xen_foro_thread_processor_test.py
+++ xen_foro_thread_processor_test.py
@@ -18,13 +18,13 @@ class TestThreadProcessorReturnsNoMessagesToSend(TestCase):
     def setUp(self):
         self.thread_getter = MagicMock()
         self.threads_from_forum = [{
-            "thread_id": "123",
+            "thread_id": "111",
             "first_message_contents": "post here"
         }]
         self.thread_getter.get_threads = MagicMock(return_value = self.threads_from_forum)
 
         self.threads_from_data_storage = [{
-            "thread_id": "123",
+            "thread_id": "111",
             "first_message_contents": "post here"
         }]
         self.thread_data_storage = MagicMock()
@@ -37,7 +37,74 @@ class TestThreadProcessorReturnsNoMessagesToSend(TestCase):
 
         assert len(actual) == 0
         self.thread_getter.get_threads.assert_called_with("http://forum", "asdfs", "123")
-        self.thread_data_storage.get_forum_thread_records({"forum_name": "my_forum", "forum_id": "123"})
+        self.thread_data_storage.get_forum_thread_records.assert_called_with({"forum_name": "my_forum", "forum_id": "123"})
 
-# main()
+class TestThreadProcessorReturnsOneMessageToSend(TestCase):
+    def setUp(self):
+        self.thread_getter = MagicMock()
+        self.threads_from_forum = [{
+            "thread_id": "111",
+            "first_message_contents": "post here"
+        },
+        {
+            "thread_id": "222",
+            "first_message_contents": "new and improved post, last one sucked really"
+        }]
+        self.thread_getter.get_threads = MagicMock(return_value = self.threads_from_forum)
+
+        self.threads_from_data_storage = [{
+            "thread_id": "111",
+            "first_message_contents": "post here"
+        }]
+        self.thread_data_storage = MagicMock()
+        self.thread_data_storage.get_forum_thread_records = MagicMock(return_value=self.threads_from_data_storage)
+
+        self.thread_processor = XenForoThreadProcessor(self.thread_getter, self.thread_data_storage, mock_config, mock_token)
+
+    def runTest(self):
+        actual = self.thread_processor.get_threads_needing_messages()
+
+        assert len(actual) == 1
+        assert actual[0] == {
+            "thread_id": "222",
+            "first_message_contents": "new and improved post, last one sucked really"
+        }
+        self.thread_getter.get_threads.assert_called_with("http://forum", "asdfs", "123")
+        self.thread_data_storage.get_forum_thread_records.assert_called_with({"forum_name": "my_forum", "forum_id": "123"})
+
+class TestThreadProcessorReturnsTwoMessagesToSendWhenNoPreviousRecorded(TestCase):
+    def setUp(self):
+        self.thread_getter = MagicMock()
+        self.threads_from_forum = [{
+            "thread_id": "111",
+            "first_message_contents": "post here"
+        },
+        {
+            "thread_id": "222",
+            "first_message_contents": "new and improved post, last one sucked really"
+        }]
+        self.thread_getter.get_threads = MagicMock(return_value = self.threads_from_forum)
+
+        self.threads_from_data_storage = []
+        self.thread_data_storage = MagicMock()
+        self.thread_data_storage.get_forum_thread_records = MagicMock(return_value=self.threads_from_data_storage)
+
+        self.thread_processor = XenForoThreadProcessor(self.thread_getter, self.thread_data_storage, mock_config, mock_token)
+
+    def runTest(self):
+        actual = self.thread_processor.get_threads_needing_messages()
+
+        assert len(actual) == 2
+        assert actual[0] == {
+            "thread_id": "111",
+            "first_message_contents": "post here"
+        }
+        assert actual[1] == {
+            "thread_id": "222",
+            "first_message_contents": "new and improved post, last one sucked really"
+        }
+        self.thread_getter.get_threads.assert_called_with("http://forum", "asdfs", "123")
+        self.thread_data_storage.get_forum_thread_records.assert_called_with({"forum_name": "my_forum", "forum_id": "123"})        
+
+        
 
