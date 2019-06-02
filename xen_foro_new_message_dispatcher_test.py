79897ca33239b86a067f89299afd35020453b082
Wheeler Weise <wheeler.weise@gmail.com>
2019-06-01 19:55:16 -0500

    in the middle of writing tests

--- xen_foro_new_message_dispatcher_test.py
+++ xen_foro_new_message_dispatcher_test.py
@@ -9,12 +9,17 @@ class Base:
         self.mock_clock_signal = MagicMock()
         self.mock_clock_signal.create_callback = MagicMock()
         self.mock_config = {
-            "update_period": "60"
+            "update_period": "60",
+            "base_url": "https://myforum.xyz/",
+            "forums": [{
+                    "forum_id": "111",
+                    "target_discord_channel": "forum posts",
+                    "message_template": "A new forum post has appeared! {thread_url}",
+                    "discord_message_emojis": []
+                }]
         }
 
         self.new_message_dispatcher = XenForoNewMessageDispatcher(self.mock_new_forum_detector, self.mock_discord_service, self.mock_discord_mention_service, self.mock_clock_signal, self.mock_config)
-
-
 
 class XenForoNewMessageDispatcherTestConstructor(TestCase, Base):
     def setUp(self):
@@ -22,4 +27,16 @@ class XenForoNewMessageDispatcherTestConstructor(TestCase, Base):
 
     def runTest(self):
         self.mock_clock_signal.create_callback.assert_called_with("60", self.new_message_dispatcher._check_for_new_threads)
+
+class XenForoNewMessageDispatcherHandlesNewlyDetectedForumPost(TestCase, Base):
+    def setUp(self):
+        Base.setUp(self)
+
+        self.new_forum_threads = [{
+            "forum_id": "111",
+            "thread_id": "123"
+        }]
+        self.mock_new_forum_detector.get_threads_needing_messages = MagicMock(return_value = self.new_forum_threads)
+        
+        self.
     
