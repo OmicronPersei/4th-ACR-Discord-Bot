2fbb44a53814610bbd60e8448e231a0524bcddfd
Wheeler Weise <wheeler.weise@gmail.com>
2019-06-03 10:28:13 -0500

    Updated dependency injector unit tests to check for the correct injection of configs

--- dependency_injection.py
+++ dependency_injection.py
@@ -9,6 +9,7 @@ from forum_thread_data_storage import ForumThreadDataStorage
 from xen_foro_request_factory import XenForoRequestFactory
 from xen_foro_thread_getter import XenForoThreadGetter
 from xen_foro_new_thread_detector import XenForoNewThreadDetector
+from xen_foro_new_message_dispatcher import XenForoNewMessageDispatcher
 
 class Dependencies:
     def __init__(self, config, secrets):
@@ -24,8 +25,10 @@ class Dependencies:
         self.welcome_message = providers.Singleton(WelcomeMessage, self.config.welcome_message, self.discord_service, self.discord_mention_factory)
         self.user_leave_notification = providers.Singleton(UserLeaveNotification, self.config.user_leave_notification, self.discord_service, self.discord_mention_factory)
         self.sql_wrapper = providers.Singleton(SQLWrapper, self.config)
-        self.forum_thread_data_storage = providers.Singleton(ForumThreadDataStorage, self.config.xen_foro_integration)
+        self.forum_thread_data_storage = providers.Singleton(ForumThreadDataStorage, self.sql_wrapper)
         self.xen_foro_request_factory = providers.Singleton(XenForoRequestFactory)
         self.xen_foro_thread_getter = providers.Singleton(XenForoThreadGetter, self.xen_foro_request_factory)
         self.xen_foro_new_thread_detector = providers.Singleton(XenForoNewThreadDetector, self.xen_foro_thread_getter, self.forum_thread_data_storage, self.config.xen_foro_integration, self.secrets.xen_foro_integration_api_token)
+        #def __init__(self, xen_foro_new_thread_detector, discord_service, discord_mention_factory, forum_thread_data_storage, clock_signal, forum_thread_url_factory, config)
+        # self.xen_foro_new_message_dispatcher = providers.Singleton(XenForoNewMessageDispatcher, self.xen_foro_new_thread_detector, self.discord_service, self.discord_mention_factory, self.forum_thread_data_storage)
 
--- dependency_injection_test.py
+++ dependency_injection_test.py
@@ -11,20 +11,34 @@ from sql_wrapper import SQLWrapper
 from xen_foro_request_factory import XenForoRequestFactory
 from xen_foro_thread_getter import XenForoThreadGetter
 from xen_foro_new_thread_detector import XenForoNewThreadDetector
+from xen_foro_new_message_dispatcher import XenForoNewMessageDispatcher
 
 def create_mock_config():
     return {
         "welcome_message": {
             "message": "the message",
-            "channel": "the channel",
+            "channel": "welcome message channel",
             "enabled": True
         },
         "user_leave_notification": {
             "message": "the message",
-            "channel": "the channel",
+            "channel": "user leave channel",
             "enabled": True
         },
-        "db_filename": ":memory:"
+        "db_filename": ":memory:",
+        "xen_foro_integration": {
+            "forum_name": "my_unique_prefix",
+            "base_url": "https://myforum.xyz/",
+            "update_period": "60",
+            "forums": [
+                {
+                    "forum_id": "234",
+                    "target_discord_channel": "forum posts",
+                    "message_template": "A new forum post has appeared! {thread_url}",
+                    "discord_message_emojis": []
+                }
+            ]
+        }
     }
 
 def create_mock_secrets():
@@ -63,16 +77,19 @@ class TestDependenciesSetsupDependencies(asynctest.TestCase):
         welcome_message_instance = welcome_message()
         assert isinstance(welcome_message, dependency_injector.providers.Singleton)
         assert isinstance(welcome_message_instance, WelcomeMessage)
+        assert welcome_message_instance._config["channel"] == "welcome message channel"
 
         user_leave_notification = self.dependencies.user_leave_notification
         user_leave_notification_instance = user_leave_notification()
         assert isinstance(user_leave_notification, dependency_injector.providers.Singleton)
         assert isinstance(user_leave_notification_instance, UserLeaveNotification)
+        assert user_leave_notification_instance._config["channel"] == "user leave channel"
 
         sql_wrapper = self.dependencies.sql_wrapper
         sql_wrapper_instance = self.dependencies.sql_wrapper()
         assert isinstance(sql_wrapper, dependency_injector.providers.Singleton)
         assert isinstance(sql_wrapper_instance, SQLWrapper)
+        assert sql_wrapper_instance._config["db_filename"] == ":memory:"
 
         forum_thread_data_storage = self.dependencies.forum_thread_data_storage
         forum_thread_data_storage_instance = self.dependencies.forum_thread_data_storage()
@@ -93,5 +110,7 @@ class TestDependenciesSetsupDependencies(asynctest.TestCase):
         xen_foro_new_thread_detector_instance = self.dependencies.xen_foro_new_thread_detector()
         assert isinstance(xen_foro_new_thread_detector, dependency_injector.providers.Singleton)
         assert isinstance(xen_foro_new_thread_detector_instance, XenForoNewThreadDetector)
+        assert xen_foro_new_thread_detector_instance._forum_config["forum_name"] == "my_unique_prefix"
+        assert xen_foro_new_thread_detector_instance._forum_api_token == "imsecret"
 
     
