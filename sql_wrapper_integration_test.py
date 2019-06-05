6fe64f0760f18aa621211a31d68000ec6ce01bb4
Wheeler Weise <wheeler.weise@gmail.com>
2019-06-05 09:11:37 -0500

    Refactored the test for reuse

--- sql_wrapper_integration_test.py
+++ sql_wrapper_integration_test.py
@@ -15,25 +15,33 @@ mock_config = {
     "db_filename": None
 }
 
-class SQLWrapperIntegrationTestChecksTablesAreSetupWhenDontExist(TestCase):
+class BaseSQLWrapperIntegrationTest(TestCase):
     def setUp(self):
         self.db = sqlite3.connect(":memory:")
         self.sql_wrapper = MockSQLWrapper(mock_config, self.db)
 
+class SQLWrapperIntegrationTestChecksTablesAreSetupWhenDontExist(BaseSQLWrapperIntegrationTest):
     def runTest(self):
         self.sql_wrapper.check_forum_has_allocated_storage()
 
         cursor = self.db.execute("select 1 from ForumMessageHistory")
         assert isinstance(cursor, sqlite3.Cursor)
 
-class SQLWrapperIntegrationTestChecksTablesAreSetupTableAlreadyExists(TestCase):
+class BaseSQLWrapperIntegrationTestWithTableAndData(BaseSQLWrapperIntegrationTest):
     def setUp(self):
-        self.db = sqlite3.connect(":memory:")
-        self.sql_wrapper = MockSQLWrapper(mock_config, self.db)
-
+        BaseSQLWrapperIntegrationTest.setUp(self)
         new_table_query = "create table ForumMessageHistory (forum_name nvarchar, forum_id nvarchar, thread_id nvarchar)"
         self.db.execute(new_table_query)
         self.db.commit()
+
+class SQLWrapperIntegrationTestChecksTablesAreSetupTableAlreadyExists(BaseSQLWrapperIntegrationTestWithTableAndData):
+    # def setUp(self):
+    #     self.db = sqlite3.connect(":memory:")
+    #     self.sql_wrapper = MockSQLWrapper(mock_config, self.db)
+
+    #     new_table_query = "create table ForumMessageHistory (forum_name nvarchar, forum_id nvarchar, thread_id nvarchar)"
+    #     self.db.execute(new_table_query)
+    #     self.db.commit()
 
     def runTest(self):
         self.sql_wrapper.check_forum_has_allocated_storage()
