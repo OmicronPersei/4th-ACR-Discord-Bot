f43784bd9ff356577454bdcc3e64120b3e1cfa26
Wheeler Weise <wheeler.weise@gmail.com>
2019-07-26 10:58:34 -0500

    Tests appear to pass.

--- discord_service.py
+++ discord_service.py
@@ -27,12 +27,13 @@ class DiscordService(discord.Client):
         matching_member = [x for x in all_members if x.name == username and x.discriminator == discriminator][0]
         return matching_member
 
+    #todo: remove?
     def get_matching_role(self, role_name):
         all_roles = self.guilds[0].roles
         return [x for x in all_roles if x.name.lower() == role_name.lower()][0]
 
-    def get_all_roles_names(self):
-        return [x.name for x in self.guilds[0].roles]
+    def get_all_roles(self):
+        return self.guilds[0].roles
 
     def create_listener_for_bot_command(self, command_prefix, callback):
         self.bot_command_callbacks[command_prefix.lower()] = callback
