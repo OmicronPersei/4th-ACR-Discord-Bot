8d812599fb2bf7129bc92a76bd09e2c0115207dc
Wheeler Weise <wheeler.weise@gmail.com>
2019-08-12 13:57:37 -0500

    Created a configuration service that will read from a file and parse its json.

---
+++ configuration_service.py
@@ -0,0 +1,18 @@
+import json
+
+class ConfigurationService:
+
+    def __init__(self, config_file_path):
+        self._config_file_path = config_file_path
+
+    def get_config_value(self, prop_name):
+        config = self._get_config_contents()
+        return config[prop_name]
+
+    def _get_config_contents(self):
+        file_contents = self._read_config_file()
+        return json.loads(file_contents)
+
+    def _read_config_file(self):
+        with open(self._config_file_path, mode='r') as f:
+            return f.read()
---
+++ configuration_service_test.py
@@ -0,0 +1,27 @@
+from configuration_service import ConfigurationService
+from asynctest import TestCase
+
+class MockConfigurationService(ConfigurationService):
+    def __init__(self, config_file_path, mock_config_file_contents):
+        super().__init__(config_file_path)
+        self.mock_config_file_contents = mock_config_file_contents
+
+    def _read_config_file(self):
+        return self.mock_config_file_contents
+
+
+class BaseTestCase:
+    def setUp(self):
+        self.mock_config_contents = r'{ "my_prop": "my_val" }'
+        self.mock_config_file_path = 'my_config.json'
+        self.mock_config_service = MockConfigurationService(self.mock_config_file_path, self.mock_config_contents)
+
+class TestGetConfigValue(BaseTestCase, TestCase):
+    def setUp(self):
+        BaseTestCase.setUp(self)
+
+    def runTest(self):
+        expected = "my_val"
+        actual = self.mock_config_service.get_config_value("my_prop")
+
+        assert actual == expected
