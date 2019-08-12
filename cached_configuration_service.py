07d39a958f093e7c287077a63796767a3a4b096a
Wheeler Weise <wheeler.weise@gmail.com>
2019-08-12 15:06:46 -0500

    First test for cached config service passes.

--- cached_configuration_service.py
+++ cached_configuration_service.py
@@ -1,8 +1,35 @@
 from configuration_service import ConfigurationService
+import time
 
 cache_expire_key = "config_cache_expires_after"
 
 class CachedConfigurationService(ConfigurationService):
     def __init__(self, config_file_path):
+        self._cache_accessed_time = None
+        self._cached_config = None
+        self._cache_timeout_seconds = None
+
         super().__init__(config_file_path)
-        self._cache_timeout_seconds = self.get_config_value(cache_expire_key)
+        
+    def _get_config_contents(self):
+        if self._is_cache_stale():
+            self.load_config_and_set_cache()
+
+        return self._cached_config
+
+    def _is_cache_stale(self):
+        if (not self._cache_accessed_time) or (not self._cache_timeout_seconds):
+            return True
+
+        cache_expires_time = (self._cache_accessed_time + self._cache_timeout_seconds)
+        current_time = self._get_current_time()
+        return current_time >= cache_expires_time
+
+    def load_config_and_set_cache(self):
+        self._cached_config = ConfigurationService._get_config_contents(self)
+        
+        self._cache_accessed_time = self._get_current_time()
+        self._cache_timeout_seconds = self._cached_config[cache_expire_key]
+    
+    def _get_current_time(self):
+        return time.time()
--- cached_configuration_service_test.py
+++ cached_configuration_service_test.py
@@ -1,27 +1,35 @@
 from cached_configuration_service import CachedConfigurationService
-from asynctest import TestCase, MagicMock
+from asynctest import TestCase, MagicMock, main
 
 class MockCachedConfigurationService(CachedConfigurationService):
     def __init__(self, config_file_path, mock_config_file_contents):
-        self._get_config_contents = MagicMock(return_value=mock_config_file_contents)
+        self._read_config_file = MagicMock(return_value=mock_config_file_contents)
         super().__init__(config_file_path)
+
+    def _get_current_time(self):
+        return self.mock_current_time()
 
 class BaseTestCase:
     def setUp(self):
-        self.mock_config_contents = { "config_cache_expires_after": 300, "my_prop": "my_val" }
+        self.mock_config_contents = r'{ "config_cache_expires_after": 300, "my_prop": "my_val" }'
         self.mock_cached_config_service = MockCachedConfigurationService(None, self.mock_config_contents)
 
-class TestReadsConfigAndRecordsFirstAccessTime(BaseTestCase, TestCase):
+class TestConfigReadsFirstTime(BaseTestCase, TestCase):
     def setUp(self):
         BaseTestCase.setUp(self)
+        self.mock_cached_config_service.mock_current_time = MagicMock(return_value=1000)
 
     def runTest(self):
-        expected = 300
-        actual = self.mock_cached_config_service._cache_timeout_seconds
-        assert actual == expected
-        
         expected = "my_val"
         actual = self.mock_cached_config_service.get_config_value("my_prop")
         assert actual == expected
 
+        expected = 300
+        actual = self.mock_cached_config_service._cache_timeout_seconds
+        assert actual == expected
         
+        expected = 1000
+        actual = self.mock_cached_config_service._cache_accessed_time
+        assert actual == expected
+
+# main()
