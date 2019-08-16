c9d99330df489b078fc0c4d092c0e6af9888d7bb
Wheeler Weise <wheeler.weise@gmail.com>
2019-08-15 21:26:29 -0500

    Modified get_matching_role_node to allow no singular root node.

--- user_reaction_report/get_matching_role_node.py
+++ user_reaction_report/get_matching_role_node.py
@@ -2,13 +2,19 @@ def get_matching_role_node(role_id, structure):
     return _find_role(role_id, structure)
 
 def _find_role(role_id, node):
-    if node["role_id"] == role_id:
-        return node
-
-    if node["children"]:
-        for child in node["children"]:
+    if isinstance(node, list):
+        for child in node:
             result = _find_role(role_id, child)
             if result is not None:
                 return result
+    else:
+        if node["role_id"] == role_id:
+            return node
+
+        if "children" in node:
+            for child in node["children"]:
+                result = _find_role(role_id, child)
+                if result is not None:
+                    return result
 
     return None
