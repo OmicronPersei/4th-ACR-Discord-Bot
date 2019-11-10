9b622e8feb7d4bb29de2fca0974d5f5188cee4b9
Wheeler Weise <wheeler.weise@gmail.com>
2019-11-10 11:55:40 -0600

    Got test to pass.

--- announcement_service.py
+++ announcement_service.py
@@ -3,3 +3,10 @@ from bot_command_service_base import BotCommandServiceBase
 class AnnouncementService(BotCommandServiceBase):
     def __init__(self, config, discord_service):
         super().__init__(config, "announcement_service", discord_service)
+
+    async def bot_command_callback(self, message):
+        msg_tokens = message.split(' ')
+        channel = msg_tokens[1]
+        announcement = ' '.join(msg_tokens[2:])
+
+        await self.discord_service.send_channel_message(announcement, channel)
