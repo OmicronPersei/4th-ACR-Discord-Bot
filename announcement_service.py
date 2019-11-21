7fc54bfc85212db2a6e43d74fe2f4af6522ffa1b
Wheeler Weise <wheeler.weise@gmail.com>
2019-11-21 13:42:28 -0600

    Updated functionality to check for matching role.

--- announcement_service.py
+++ announcement_service.py
@@ -1,12 +1,24 @@
 from bot_command_service_base import BotCommandServiceBase
 
+service_name = "announcement_service"
+
 class AnnouncementService(BotCommandServiceBase):
     def __init__(self, config, discord_service):
-        super().__init__(config, "announcement_service", discord_service)
+        super().__init__(config, service_name, discord_service)
 
     async def bot_command_callback(self, message):
+        if not self.user_has_allowed_role(message):
+            return
+
         msg_tokens = message.content.split(' ')
         channel = msg_tokens[1]
         announcement = ' '.join(msg_tokens[2:])
 
         await self.discord_service.send_channel_message(announcement, channel)
+
+    def user_has_allowed_role(self, message):
+        allowed_roles = set(self.config.get(service_name)["allowed_roles"])
+        for role in message.author.roles:
+            if role.id in allowed_roles:
+                return True
+        return False
--- announcement_service_test.py
+++ announcement_service_test.py
@@ -1,15 +1,38 @@
-from announcement_service import AnnouncementService
+from announcement_service import AnnouncementService, service_name
 from discord_service import DiscordService
 from asynctest import TestCase, MagicMock, Mock
 from asyncio import Future
 
-from test_utils import MockConfigurationService, create_mock_message
+from test_utils import MockConfigurationService, create_mock_message, create_mock_role
 
-class TestMakeAnnouncement(TestCase):
+class TestMakeAnnouncementUserHasRole(TestCase):
+    def setUp(self):
+        mock_config_obj = {
+            service_name: {
+                "command_keyword": "!announce",
+                "allowed_roles": [ 12345 ]
+            }
+        }
+
+        self.mock_config = MockConfigurationService(mock_config_obj)
+
+        self.mock_discord_service = Mock(DiscordService)
+
+        self.announcement_service = AnnouncementService(self.mock_config, self.mock_discord_service)
+
+    async def runTest(self):
+        mock_role = create_mock_role(12345, "MyRole")
+        mock_message = create_mock_message("!announce operations This is an operation announcement", "the chan", [ mock_role ])
+        await self.announcement_service.bot_command_callback(mock_message)
+
+        self.mock_discord_service.send_channel_message.assert_called_with("This is an operation announcement", "operations")
+
+class TestMakeAnnouncementUserDoesNotHaveRole(TestCase):
     def setUp(self):
         mock_config_obj = {
             "announcement_service": {
-                "command_keyword": "!announce"
+                "command_keyword": "!announce",
+                "allowed_roles": [ 12345 ]
             }
         }
 
@@ -23,4 +46,4 @@ class TestMakeAnnouncement(TestCase):
         mock_message = create_mock_message("!announce operations This is an operation announcement", "the chan")
         await self.announcement_service.bot_command_callback(mock_message)
 
-        self.mock_discord_service.send_channel_message.assert_called_with("This is an operation announcement", "operations")
+        self.mock_discord_service.send_channel_message.assert_not_called()
