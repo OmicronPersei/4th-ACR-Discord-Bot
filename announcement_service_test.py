8fa6e20418815d520428058175e0363b6e8d2125
Wheeler Weise <wheeler.weise@gmail.com>
2019-11-25 19:26:15 -0600

    Refactored tests to have a common base.

--- announcement_service_test.py
+++ announcement_service_test.py
@@ -5,7 +5,7 @@ from asyncio import Future
 
 from test_utils import MockConfigurationService, create_mock_message, create_mock_role
 
-class TestMakeAnnouncementUserHasRole(TestCase):
+class TestAnnouncementBase:
     def setUp(self):
         mock_config_obj = {
             service_name: {
@@ -20,6 +20,11 @@ class TestMakeAnnouncementUserHasRole(TestCase):
 
         self.announcement_service = AnnouncementService(self.mock_config, self.mock_discord_service)
 
+
+class TestMakeAnnouncementUserHasRole(TestAnnouncementBase, TestCase):
+    def setUp(self):
+        TestAnnouncementBase.setUp(self)
+
     async def runTest(self):
         mock_role = create_mock_role(12345, "MyRole")
         mock_message = create_mock_message("!announce create operations This is an operation announcement", "the chan", [ mock_role ])
@@ -27,23 +32,15 @@ class TestMakeAnnouncementUserHasRole(TestCase):
 
         self.mock_discord_service.send_channel_message.assert_called_with("This is an operation announcement", "operations")
 
-class TestMakeAnnouncementUserDoesNotHaveRole(TestCase):
+class TestMakeAnnouncementUserDoesNotHaveRole(TestAnnouncementBase, TestCase):
     def setUp(self):
-        mock_config_obj = {
-            "announcement_service": {
-                "command_keyword": "!announce",
-                "allowed_roles": [ 12345 ]
-            }
-        }
-
-        self.mock_config = MockConfigurationService(mock_config_obj)
-
-        self.mock_discord_service = Mock(DiscordService)
-
-        self.announcement_service = AnnouncementService(self.mock_config, self.mock_discord_service)
+        TestAnnouncementBase.setUp(self)
 
     async def runTest(self):
         mock_message = create_mock_message("!announce create operations This is an operation announcement", "the chan")
         await self.announcement_service.bot_command_callback(mock_message)
 
         self.mock_discord_service.send_channel_message.assert_not_called()
+
+# class TestEditAnouncement(TestCase):
+#     def setUp(self):
