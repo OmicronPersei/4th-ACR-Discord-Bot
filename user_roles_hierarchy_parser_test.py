f275b24514f8f0c682486332ba3ab5e34169fbb6
Wheeler Weise <wheeler.weise@gmail.com>
2020-05-02 16:46:04 -0500

    refactored to allow roleless objs

--- user_roles_hierarchy_parser_test.py
+++ user_roles_hierarchy_parser_test.py
@@ -6,23 +6,39 @@ from user_roles_hierarchy_parser import create_roles_dictionary
 mock_config = json.loads("""
 [
     {
-        "role": "11111"
+        "role": "1"
     },
     {
-        "role": "22222",
-        "sub_roles_access_channel": "33333",
+        "role": "2",
+        "sub_roles_access_channel": "22222",
         "sub_roles": [
             {
-                "role": "44444"
+                "role": "21"
             },
             {
-                "role": "55555",
-                "sub_roles_access_channel": "66666",
+                "role": "22",
+                "sub_roles_access_channel": "33333",
                 "sub_roles": [
                     {
-                        "role": "77777"
+                        "role": "221"
                     }
                 ]
+            },
+            {
+                "sub_roles_access_channel": "44444",
+                "sub_roles": [
+                    {
+                        "role": "3"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "sub_roles_access_channel": "55555",
+        "sub_roles": [
+            {
+                "role": "4"
             }
         ]
     }
@@ -40,19 +56,29 @@ class TestGetSubRolesInTopLevelOfDictionary(TestCase):
         default_channel_roles = channel_roles_dict[self.default_channel]
 
         assert len(default_channel_roles) == 2
-        assert "11111" in default_channel_roles
-        assert "22222" in default_channel_roles
+        assert "1" in default_channel_roles
+        assert "2" in default_channel_roles
+
+        assert "22222" in channel_roles_dict
+        two_sub_roles = channel_roles_dict["22222"]
+        assert len(two_sub_roles) == 2
+        assert "21" in two_sub_roles
+        assert "22" in two_sub_roles
 
         assert "33333" in channel_roles_dict
         three_sub_roles = channel_roles_dict["33333"]
-        assert len(three_sub_roles) == 2
-        assert "44444" in three_sub_roles
-        assert "55555" in three_sub_roles
+        assert len(three_sub_roles) == 1
+        assert "221" in three_sub_roles
 
-        assert "66666" in channel_roles_dict
-        six_sub_roles = channel_roles_dict["66666"]
-        assert len(six_sub_roles) == 1
-        assert "77777" in six_sub_roles
+        assert "44444" in channel_roles_dict
+        four_sub_roles = channel_roles_dict["44444"]
+        assert len(four_sub_roles) == 1
+        assert "3" in four_sub_roles
+
+        assert "55555" in channel_roles_dict
+        five_sub_roles = channel_roles_dict["55555"]
+        assert len(five_sub_roles) == 1
+        assert "4" in five_sub_roles
 
 class TestGettingRolesForUnsupportedChannelThrows(TestCase):
     def setUp(self):
